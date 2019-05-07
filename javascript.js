c4fdc59f35a067b06399091aba3ce29ae0b13c28
tspalter <tspalter@wustl,edu>
2019-05-06 23:41:46 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -29,8 +29,11 @@ var multiplier = 1;
 var tableLimit, reverse_tableLimit, moveUpLeft, moveUpRight,
   moveDownLeft, moveDownRight, tableLimitLeft, tableLimitRight;
 
-var p1Playing = false;
-var p2Playing = false;
+var p1name = "\u00D7";
+var p2name = "\u00D7";
+var p1Win = false;
+var p2Win = false;
+
 
 //Find the move distance based on window size
 getDimension();
@@ -525,12 +528,84 @@ function declareWinner() {
   score.style.display = "block";
 
   if (the_checker[1].color == "red") {
-    score.innerHTML = "Black Wins!";
-  } else {
-    score.innerHTML = "Red Wins!";
+    if (p2name != "\u00D7") {
+      score.innerHTML = p2name + " Wins!";
+      p2Win = true;
+      let createRequest = new XMLHttpRequest();
+
+      createRequest.onreadystatechange = function() {
+        //wait for readyState = 4 & 200 response
+        if (this.readyState == 4 && this.status == 200) {
+          //parse JSON
+          var player = JSON.parse(this.responseText);
+          winCount(player);
+        }
+      };
+    }
+    else {
+      score.innerHTML = "Black Wins!";
+    }
+  }
+  else {
+    if (p1name != "\u00D7") {
+      score.innerHTML = p1name + " Wins!";
+      p1Win = true;
+      let createRequest = new XMLHttpRequest();
+
+      createRequest.onreadystatechange = function() {
+        //wait for readyState = 4 & 200 response
+        if (this.readyState == 4 && this.status == 200) {
+          //parse JSON
+          var player = JSON.parse(this.responseText);
+          console.log(player);
+          winCount(player);
+        }
+      };
+    }
+    else {
+      score.innerHTML = "Red Wins!";
+    }
   }
 }
 
+function winCount(playersData) {
+  let createRequest = new XMLHttpRequest();
+  if (p1Win) {
+    for (var i = 0; i < playersData.player.length; i++) {
+      if (playersData.player[i].name == p1name) {
+        playersData.player[i].wins++;
+      }
+    }
+    if (p2name != "\u00D7") {
+      for (var i = 0; i < playersData.player.length; i++) {
+        if (playersData.player[i].name == p2name) {
+          playersData.player[i].losses++;
+        }
+      }
+    }
+    p1Win = false;
+  }
+  else if (p2Win) {
+    for (var i = 0; i < playersData.player.length; i++) {
+      if (playersData.player[i].name == p2name) {
+        playersData.player[i].wins++;
+      }
+    }
+    if (p1name != "\u00D7") {
+      for (var i = 0; i < playersData.player.length; i++) {
+        if (playersData.player[i].name == p1name) {
+          playersData.player[i].losses++;
+        }
+      }
+    }
+    p2Win = false;
+  }
+  createRequest.open("PUT", "https://api.jsonbin.io/b/5ccfb25a4e6508572731c933", true);
+  createRequest.setRequestHeader("Content-type", "application/json");
+  createRequest.setRequestHeader("secret-key", "$2a$10$f4dPyKG5gkCRq8mJ1CfubOzdCVosqm5NLju6WZ5lW8tjJSVZ6vT36");
+  createRequest.setRequestHeader("versioning", "false");
+  createRequest.send(JSON.stringify(playersData));
+}
 
 //Find the size of the window
 function getDimension() {
@@ -560,6 +635,7 @@ document.getElementById('p1').addEventListener("submit", function(event) {
       var player = JSON.parse(this.responseText);
       // addPlayer1(player);
       var found = false;
+      p1name = player1Input.value;
       for (var p of player.player) {
         if (p.name === player1Input.value) {
           found = true;
@@ -575,7 +651,45 @@ document.getElementById('p1').addEventListener("submit", function(event) {
       console.log(this.responseText);
     }
   };
-  createRequest.open("GET", "https://api.jsonbin.io/b/5ccfb25a4e6508572731c933/7", true);
+  createRequest.open("GET", "https://api.jsonbin.io/b/5ccfb25a4e6508572731c933/9", true);
+  createRequest.setRequestHeader("secret-key", "$2a$10$f4dPyKG5gkCRq8mJ1CfubOzdCVosqm5NLju6WZ5lW8tjJSVZ6vT36");
+  createRequest.setRequestHeader("versioning", "false");
+  createRequest.send();
+});
+
+//Handle new PLayer2 form submit
+document.getElementById('p2').addEventListener("submit", function(event) {
+  console.log(event);
+  event.preventDefault();
+  var data = {
+    name: player2Input.value
+  }
+  //Initialize AJAX
+  let createRequest = new XMLHttpRequest();
+
+  createRequest.onreadystatechange = function() {
+    //wait for readyState = 4 & 200 response
+    if (this.readyState == 4 && this.status == 200) {
+      //parse JSON
+      var player = JSON.parse(this.responseText);
+      var found = false;
+      p2name = player2Input.value;
+      for (var p of player.player) {
+        if (p.name === player2Input.value) {
+          found = true;
+          break;
+        }
+      }
+      if (!found) {
+        //Create player
+        createPlayer2(player);
+      }
+      addPlayer2(player);
+    } else if (this.readyState == 4) {
+      console.log(this.responseText);
+    }
+  };
+  createRequest.open("GET", "https://api.jsonbin.io/b/5ccfb25a4e6508572731c933/9", true);
   createRequest.setRequestHeader("secret-key", "$2a$10$f4dPyKG5gkCRq8mJ1CfubOzdCVosqm5NLju6WZ5lW8tjJSVZ6vT36");
   createRequest.setRequestHeader("versioning", "false");
   createRequest.send();
@@ -591,14 +705,50 @@ function createPlayer1(playersData) {
   createRequest.setRequestHeader("versioning", "false");
   createRequest.send(JSON.stringify(playersData));
 }
+
+function createPlayer2(playersData) {
+  var createRequest = new XMLHttpRequest();
+  var newPlayer = {name: player2Input.value, wins: 0, losses: 0};
+  playersData.player.push(newPlayer);
+  createRequest.open("PUT", "https://api.jsonbin.io/b/5ccfb25a4e6508572731c933", true);
+  createRequest.setRequestHeader("Content-type", "application/json");
+  createRequest.setRequestHeader("secret-key", "$2a$10$f4dPyKG5gkCRq8mJ1CfubOzdCVosqm5NLju6WZ5lW8tjJSVZ6vT36");
+  createRequest.setRequestHeader("versioning", "false");
+  createRequest.send(JSON.stringify(playersData));
+}
+
 //add Player1 to game
 function addPlayer1(playersData) {
   var playerText = document.createElement("p");
-  console.log(playersData.player.name);
-  playerText.innerText = playersData.player.name; //+ ": " + player1Data.wins + "-" + player1Data.losses;
+  var p1data = "";
+  for (var i = 0; i < playersData.player.length; i++) {
+    if (playersData.player[i].name == player1Input.value) {
+        p1name = playersData.player[i].name;
+        p1data = playersData.player[i].name + ": " + playersData.player[i].wins + "-" + playersData.player[i].losses;
+    }
+  }
+  console.log(p1name);
+  playerText.innerText = p1data;
   document.getElementById("p1").appendChild(playerText);
   document.getElementById("player1Input").style.display = "none";
   document.getElementById("add-player1").style.display = "none";
+}
+
+//add Player2 to game
+function addPlayer2(playersData) {
+  var playerText = document.createElement("p");
+  var p2data = "";
+  for (var i = 0; i < playersData.player.length; i++) {
+    if (playersData.player[i].name == player2Input.value) {
+        p2name = playersData.player[i].name;
+        p2data = playersData.player[i].name + ": " + playersData.player[i].wins + "-" + playersData.player[i].losses;
+    }
+  }
+  console.log(p2name);
+  playerText.innerText = p2data;
+  document.getElementById("p2").appendChild(playerText);
+  document.getElementById("player2Input").style.display = "none";
+  document.getElementById("add-player2").style.display = "none";
 }
 
 document.getElementsByTagName("BODY")[0].onresize = function() {
